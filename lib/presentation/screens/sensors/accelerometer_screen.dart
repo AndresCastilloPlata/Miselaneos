c9336e8d204d61b55ceda711ab4eb1f068950bb4
AndresCastilloPlata <plataandres26@gmail.com>
2025-02-26 09:26:04 -0500

    se realiza la integracion del provider con la accelerometer_screen y muestre sus valores en pantalla

--- lib/presentation/screens/sensors/accelerometer_screen.dart
+++ lib/presentation/screens/sensors/accelerometer_screen.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:permissions_app/presentation/providers/providers.dart';
 
-class AccelerometerScreen extends StatelessWidget {
+class AccelerometerScreen extends ConsumerWidget {
   const AccelerometerScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final accelerometer = ref.watch(accelerometerUserProvider);
+
     return Scaffold(
       appBar: AppBar(title: const Text('Acelerómetro')),
-      body: const Center(child: Text('aquiles va')),
+      body: Center(
+        child: accelerometer.when(
+          data:
+              (value) =>
+                  Text(value.toString(), style: const TextStyle(fontSize: 30)),
+          error: (error, stackTrace) => Text('Error: $error'),
+          loading: () => const CircularProgressIndicator(),
+        ),
+      ),
     );
   }
 }
