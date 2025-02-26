15d41106cbc4193fc1c5b0fa22f15a5233102c71
AndresCastilloPlata <plataandres26@gmail.com>
2025-02-26 10:21:53 -0500

    instalacion de dependencia flutter_compass para la conversion de valores, uso de provider para redirigir si no se ha otorgado permiso

--- lib/presentation/screens/sensors/compass_screen.dart
+++ lib/presentation/screens/sensors/compass_screen.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:permissions_app/presentation/providers/providers.dart';
+import 'package:permissions_app/presentation/screens/screens.dart';
 
-class CompassScreen extends StatelessWidget {
+class CompassScreen extends ConsumerWidget {
   const CompassScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final locationGranted = ref.watch(permissionsProvider).locationGranted;
+
+    if (!locationGranted) {
+      return const AskLocationScreen();
+    }
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Compass')),
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text('Brújula', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.black,
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
       body: const Center(child: Text('aquiles va')),
     );
   }
