d26b10dd5fb929973a8bf0393475e22b90d9dd50
AndresCastilloPlata <plataandres26@gmail.com>
2025-02-26 09:32:16 -0500

    se realiza la integracion del provider con la marnometer_screen y muestre sus valores en pantalla

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -4,3 +4,4 @@ export 'permissions/permissions_provider.dart';
 
 export 'sensors/gyroscope_provider.dart';
 export 'sensors/accelerometer_provider.dart';
+export 'sensors/magnometer_provider.dart';
--- lib/presentation/screens/sensors/magnometer_screen.dart
+++ lib/presentation/screens/sensors/magnometer_screen.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:permissions_app/presentation/providers/providers.dart';
 
-class MagnetometerScreen extends StatelessWidget {
+class MagnetometerScreen extends ConsumerWidget {
   const MagnetometerScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final magnetometer = ref.watch(magnetometerProvider);
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Magnometer')),
-      body: const Center(child: Text('aquiles va')),
+      appBar: AppBar(title: const Text('Magnetómetro')),
+      body: Center(
+        child: magnetometer.when(
+          data:
+              (value) => Text(
+                value.x.toString(),
+                style: const TextStyle(fontSize: 30),
+              ),
+          error: (error, stackTrace) => Text('Error: $error'),
+          loading: () => const CircularProgressIndicator(),
+        ),
+      ),
     );
   }
 }
