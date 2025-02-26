619f4b5f70431cdac16ad8ee68c2aa826c28789b
AndresCastilloPlata <plataandres26@gmail.com>
2025-02-26 08:56:22 -0500

    creaccion de pantallas y ajuste de rutas para la navegacion

--- lib/Config/router/app_router.dart
+++ lib/Config/router/app_router.dart
@@ -2,11 +2,29 @@ import 'package:go_router/go_router.dart';
 import 'package:permissions_app/presentation/screens/screens.dart';
 
 final router = GoRouter(
+  //* General
   routes: [
     GoRoute(path: '/', builder: (context, state) => HomeScreen()),
     GoRoute(
       path: '/permissions',
       builder: (context, state) => PermissionsScreen(),
     ),
+
+    //* Sensors
+    //* Sensors
+    GoRoute(path: '/gyroscope', builder: (context, state) => GyroscopeScreen()),
+    GoRoute(
+      path: '/accelerometer',
+      builder: (context, state) => AccelerometerScreen(),
+    ),
+    GoRoute(
+      path: '/magnetometer',
+      builder: (context, state) => MagnetometerScreen(),
+    ),
+    GoRoute(
+      path: '/gyroscope_ball',
+      builder: (context, state) => GyroscopeBallScreen(),
+    ),
+    GoRoute(path: '/compass', builder: (context, state) => CompassScreen()),
   ],
 );
