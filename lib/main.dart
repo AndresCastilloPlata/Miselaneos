bacfa3bf99e2c72469c1d070c50b9978e74d9c41
AndresCastilloPlata <plataandres26@gmail.com>
2025-02-26 09:01:04 -0500

    bloqueo de orientacion de la pantalla para que se mantenga vertical

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:permissions_app/Config/config.dart';
 import 'package:permissions_app/presentation/providers/providers.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(ProviderScope(child: const MainApp()));
 }
 
