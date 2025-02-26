061b9bf17213814725438655262a0bb8d754538f
AndresCastilloPlata <plataandres26@gmail.com>
2025-02-26 09:55:03 -0500

    se realiza la integracion del provider con la gyroscope_ball_screen y muestre sus valores en pantalla

--- lib/presentation/screens/sensors/gyroscope_ball_screen.dart
+++ lib/presentation/screens/sensors/gyroscope_ball_screen.dart
@@ -1,13 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:permissions_app/presentation/providers/providers.dart';
 
-class GyroscopeBallScreen extends StatelessWidget {
+class GyroscopeBallScreen extends ConsumerWidget {
   const GyroscopeBallScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final gyroscope = ref.watch(gyroscopeProvider);
+
     return Scaffold(
       appBar: AppBar(title: const Text('Gyroscope Ball')),
-      body: const Center(child: Text('aquiles va')),
+
+      body: SizedBox.expand(
+        child: gyroscope.when(
+          data: (value) => MovingBall(x: value.x, y: value.y),
+          error: (error, stackTrace) => Text('Error: $error'),
+          loading: () => const CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
+
+class MovingBall extends StatelessWidget {
+  final double x;
+  final double y;
+
+  const MovingBall({super.key, required this.x, required this.y});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        const Ball(),
+        Text('''
+X: $x,
+Y: $y,
+      ''', style: TextStyle(fontSize: 30)),
+      ],
+    );
+  }
+}
+
+class Ball extends StatelessWidget {
+  const Ball({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration(
+        color: Colors.blueAccent,
+        borderRadius: BorderRadius.circular(100),
+      ),
     );
   }
 }
