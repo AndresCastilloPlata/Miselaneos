00535625cf25f09a5d0965b35f24eb2683a3648c
AndresCastilloPlata <plataandres26@gmail.com>
2025-03-03 18:58:17 -0500

    se crea provider para dar ubicacion actual del usuario, se ajusta para consumir el provider

--- lib/presentation/screens/location/location_screen.dart
+++ lib/presentation/screens/location/location_screen.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:permissions_app/presentation/providers/providers.dart';
 
-class LocationScreen extends StatelessWidget {
+class LocationScreen extends ConsumerWidget {
   const LocationScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('LocationScreen')));
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userLocationAsync = ref.watch(userLocationProvider);
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Ubicación')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 30,
+          children: [
+            // Current location
+            const Text('Ubicación actual'),
+            userLocationAsync.when(
+              data: (data) => Text('$data'),
+              error: (error, stackTrace) => Text('Error: $error'),
+              loading: () => const CircularProgressIndicator(),
+            ),
+            // Current location
+            const Text('Seguimiento de Ubicación'),
+          ],
+        ),
+      ),
+    );
   }
 }
